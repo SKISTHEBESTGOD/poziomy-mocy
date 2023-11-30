b7b0d080d505ceda603805ba641818c11d80e2be
SKISTHEBESTGOD <skiperersiema@gmail.com>
2023-11-30 19:52:24 +0100

    <Update 2.0>  Program obliczający poziomy ucznia i inne statystyki

--- src/samparamingo.java
+++ src/samparamingo.java
@@ -7,22 +7,33 @@ class samparamingo {
         double percent = drukarka.nextDouble();
         double poziom;
         double wartosc;
+        String poziomNazwa = "";
         if(percent < 0 || percent > 100) {
             System.out.println("Nieprawidłowe dane");
         }
         else {
             poziom = Math.pow(percent,2) / 10000 * 5;
+            if(poziom < 2) {
+                poziomNazwa = "Casual";
+            }
+            else if(poziom >= 2 && poziom < 3) {
+                poziomNazwa = "Tribunal";
+            }
+            else if(poziom >= 3 && poziom < 4) {
+                poziomNazwa = "Dragon";
+            }
+            else if(poziom >= 4 && poziom < 5) {
+                poziomNazwa = "Godly";
+            }
+            else {
+                poziomNazwa = "SK";
+            }
             wartosc =  100/(100-percent);
             System.out.println("Twój procent doskonałości to: " + percent + "%");
 
-            if(poziom == 5) {
-                System.out.println("Twój poziom ucznia to: SK ");
-                System.out.println("Twoja wartość mocy to: SK ");
-            }
-            else {
-                System.out.println("Twój poziom ucznia to: " + poziom);
+                System.out.println("Twój poziom ucznia to: " + poziom + "(" +poziomNazwa +")");
                 System.out.println("Twoja wartość mocy to: " + wartosc + "p");
-            }
+
             double mnoznik = wartosc / 2;
             if(wartosc >= 2) {
                 System.out.println("Jesteś potężniejszy od przeciętnego człowieka " + mnoznik + " razy!!");
